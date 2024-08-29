a6afe52971791e4beea3ae79804c03d36526b829
Leonardo Barbosa Barros <leebbarross@gmail.com>
2024-08-29 17:45:51 -0300

    subindo classes

---
+++ scr/br/ufal/poo/Restaurante.java
@@ -0,0 +1,7 @@
+package scr.br.ufal.poo;
+
+public class Restaurante extends Empresa {
+    public Restaurante(String nome, String endereco, String tipoCozinha) {
+        super(nome, endereco, tipoCozinha);
+    }
+}
