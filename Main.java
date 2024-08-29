66e27c8e76f0119253369f026d97936879b2d582
Leonardo Barbosa Barros <leebbarross@gmail.com>
2024-08-29 16:24:42 -0300

    subindo classes

---
+++ Main.java
@@ -0,0 +1,12 @@
+import easyaccept.EasyAccept;
+
+public class Main {  // Nome da classe alterado para 'Main'
+    public static void main(String[] args) {
+        // Caminho para a classe de fachada e para os arquivos de teste
+        args = new String[] {
+                "scr.br.ufal.poo.Facade",   // Nome completo da classe de fachada, incluindo o pacote
+                "testes/us1_1.txt"     // Caminho relativo para o arquivo de teste
+        };
+        EasyAccept.main(args);
+    }
+}
