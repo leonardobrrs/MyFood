604cdf9d6bc7cd297d8d0800b131203fee455d7c
herberty <herbertylive@gmail.com>
2024-08-31 14:22:07 -0300

    gv

---
+++ src/br/ufal/ic/p2/myfood/Produto.java
@@ -0,0 +1,34 @@
+package br.ufal.ic.p2.myfood;
+
+import br.ufal.ic.p2.myfood.Exceptions.AtributoInvalidoException;
+
+public class Produto {
+    private static int idCounter = 0; // Gerador de ID automático
+    private int id;
+    private String nome;
+    private float valor;
+    private String categoria;
+
+    public Produto(String nome, float valor, String categoria) {
+        this.id = ++idCounter;
+        this.nome = nome;
+        this.valor = valor;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+}
